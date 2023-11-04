877c827f2cbaf8f9e9a16362a9615f1b57bd0433
Ryan Alexander Partington <pinescore@outlook.com>
2023-11-04 10:56:59 +0000

    disables when in instances

--- NPC_HighLevelAlert.lua
+++ NPC_HighLevelAlert.lua
@@ -1,4 +1,4 @@
--- core.lua Version 3
+-- core.lua Version 4
 
 local frame = CreateFrame("Frame")
 local lastAlertTime = 0  -- Initialize a variable to store the time of the last alert
@@ -7,8 +7,13 @@ local lastAlertTime = 0  -- Initialize a variable to store the time of the last 
 frame:RegisterEvent("NAME_PLATE_UNIT_ADDED")
 
 frame:SetScript("OnEvent", function(self, event, unit)
+    local inInstance, instanceType = IsInInstance()  -- Check if the player is in an instance (Version 2)
+    if inInstance then
+        return  -- Exit the function early if inside an instance (Version 2)
+    end
+    
     local currentTime = GetTime()  -- Get the current time
-    if currentTime - lastAlertTime >= 60 then  -- Check if at least 5 seconds have passed since the last alert
+    if currentTime - lastAlertTime >= 60 then  -- Check if at least 60 seconds have passed since the last alert
         -- Make sure the unit is an NPC and is an enemy
         if not UnitIsPlayer(unit) and UnitIsEnemy("player", unit) then
             -- Get the unit's level and classification
@@ -20,7 +25,7 @@ frame:SetScript("OnEvent", function(self, event, unit)
             local isElite = npcClassification == "elite" or npcClassification == "rareelite" or npcClassification == "worldboss"
             
             -- Check if the unit's level is higher than the player's level or if the unit is elite
-            if npcLevel >= playerLevel + 1 or isElite then
+            if npcLevel >= playerLevel + 3 or isElite then
                 local npcName = UnitName(unit)
                 -- Display a raid warning and play a sound
                 RaidNotice_AddMessage(RaidWarningFrame, (isElite and npcClassification.." " or "")..npcName.."("..npcLevel..") close", ChatTypeInfo["RAID_WARNING"])
